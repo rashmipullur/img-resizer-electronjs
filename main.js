7c3b31dffe59bad69f8f9d2c7b521b8de2ee9b27
rashmipullur <mheresince1998@gmail.com>
2024-04-25 13:05:50 +0530

    show devtools

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const path = require("path");
 const { app, BrowserWindow } = require("electron");
 
+const isDev = process.env.NODE_ENV !== "production";
 const isMac = process.platform === "darwin";
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "Image  Resizer",
-    width: 500,
+    width: isDev ? 1000 : 500,
     height: 600,
   });
+
+  // open dev tools if in development
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
