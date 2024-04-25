1e97bc9fd32d3b6a31877edbaf5e95ec4b1b0c8b
rashmipullur <mheresince1998@gmail.com>
2024-04-25 11:45:01 +0530

    common boilerplate

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const { app, BrowserWindow } = require("electron");
 
+const isMac = process.platform === "darwin";
+
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "Image  Resizer",
@@ -13,5 +15,16 @@ function createMainWindow() {
 
 app.whenReady().then(() => {
   createMainWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
 });
 
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
